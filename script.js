8d566240b96615ff616ba367a2ee1d35ac4ae1d5
mithrandiryeet <122556370+mithrandiryeet@users.noreply.github.com>
2023-03-20 10:35:47 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //script file
+
+// const links = document.querySelectorAll('a'); // get all the links on the page
+
+// links.forEach(link => {
+//   link.addEventListener('click', () => {
+//     console.log(`Link clicked: ${link.href}`); // log the href attribute of the clicked link
+//   });
+// });
+
+// function log() {
+//     console.log('Clicked');
+//   };
+//   <button onclick="log()">Click me!</button>
+
+// console.log('https://vikasnehra1.github.io/CharlieLab/');
